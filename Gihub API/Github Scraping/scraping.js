71f51fd09bbe1f12f2ccb36c22762b43b88a1910
edugieun <gieun625@gmail.com>
2019-12-19 01:37:29 +0900

    191218 | feature/source code scarping

--- Gihub API/Github Scraping/scraping.js
+++ Gihub API/Github Scraping/scraping.js
@@ -1,8 +1,80 @@
-const axios = require('axios')
-axios.get('http://jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    console.log(response)
+// const axios = require('axios')
+// axios.get('http://jsonplaceholder.typicode.com/posts')
+//   .then(response => {
+//     console.log(response)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+
+
+var request = require("request");
+// let ab
+// ab = request({
+//   url: "https://github.com/edugieun/Algorithm-Solving/blob/master/Array/0000_Bomber1(D3%2C%20Matrix)/bomber1.py",
+// }, function(error, response, body) {
+//   let start = 0
+//   let end = 0
+//   for (let i = 0; i < body.length; i++) {
+//     if (body[i] === '<' && body[i+1] === 't' && body[i+2] === 'a' && body[i+3] === 'b' && body[i+4] === 'l') {
+//       start = i
+//     }
+//     if (body[i] === '/' && body[i+1] === 't' && body[i+2] === 'a' && body[i+3] === 'b' && body[i+4] === 'l') {
+//       end = i + 8
+//       let table_ele = ''
+//       for (i = start; i < end; i++) {
+//         table_ele = table_ele + body[i]
+//       }
+//       return table_ele
+//       // console.log(table_ele)
+//       // break
+//     }
+//   }
+// })
+// console.log(ab)
+
+// function getpage(link, callback){
+//   var options = {
+//   method: 'GET',
+//   uri: link,
+//   };
+//   request(options, function (err, response, body) {
+//      callback(body);
+//   });
+// }
+
+// function getdata(data){
+//   console.log(data)
+// }
+
+// // getpage("https://github.com/edugieun/Algorithm-Solving/blob/master/Array/0000_Bomber1(D3%2C%20Matrix)/bomber1.py", console.log)
+// getpage("https://github.com/edugieun/Algorithm-Solving/blob/master/Array/0000_Bomber1(D3%2C%20Matrix)/bomber1.py", getdata)
+
+
+function getpage(link, callback){
+  request({
+    url: link,
+  }, function(error, response, body) {
+    let start = 0
+    let end = 0
+    for (let i = 0; i < body.length; i++) {
+      if (body[i] === '<' && body[i+1] === 't' && body[i+2] === 'a' && body[i+3] === 'b' && body[i+4] === 'l') {
+        start = i
+      }
+      if (body[i] === '/' && body[i+1] === 't' && body[i+2] === 'a' && body[i+3] === 'b' && body[i+4] === 'l') {
+        end = i + 8
+        let table_ele = ''
+        for (i = start; i < end; i++) {
+          table_ele = table_ele + body[i]
+        }
+        callback(table_ele)
+      }
+    }
   })
-  .catch(err => {
-    console.log(err)
-  })
+}
+
+function getdata(data){
+  console.log(data)
+}
+
+getpage("https://github.com/edugieun/Algorithm-Solving/blob/master/Array/0000_Bomber1(D3%2C%20Matrix)/bomber1.py", getdata)
--- frontend/src/views/AlgoDetail.vue
+++ frontend/src/views/AlgoDetail.vue
@@ -1,30 +1,81 @@
 <template>
-  <div>
-    {{ selectedProblem }}
+
+  <div v-html="codeHtml">
+    {{ codeHtml }}
   </div>
-  
 </template>
 
-<script scoped>
+<script>
   export default {
     name: 'AlgoDetail',
     data() {
       return {
-        selectedProblem: JSON.parse(localStorage.getItem('localproblem'))
+        selectedProblem: JSON.parse(localStorage.getItem('localproblem')),
+        codeHtml: 'ad'
       }
     },
-    mehtods: {
+    methods: {
+      getdata: function (data) {
+        console.log(data)
+        this.codeHtml = data
+      }
     },
     mounted() {
-      // const request = require('request')
-      // const selectedProblem = JSON.parse(localStorage.getItem('localproblem'))
-      // console.log(3)
-      // let xhr = new XMLHttpRequest()
-      // xhr.open("GET", 'https://github.com/edugieun/Algorithm-Solving/blob/master/Array/0000_Bomber1(D3%2C%20Matrix)/bomber1.py', false)
-      // xhr.send()
-      // console.log(xhr.load('https://github.com/edugieun/Algorithm-Solving/blob/master/Array/0000_Bomber1(D3%2C%20Matrix)/bomber1.py'))
-      // console.log(xhr)}
+      var request = require("request");
+
+      function getpage(link, callback) {
+        request({
+          url: link,
+        }, function (error, response, body) {
+          let start = 0
+          let end = 0
+          for (let i = 0; i < body.length; i++) {
+            if (body[i] === '<' && body[i + 1] === 't' && body[i + 2] === 'a' && body[i + 3] === 'b' && body[i +
+              4] === 'l') {
+              start = i
+            }
+            if (body[i] === '/' && body[i + 1] === 't' && body[i + 2] === 'a' && body[i + 3] === 'b' && body[i +
+              4] === 'l') {
+              end = i + 8
+              let table_ele = ''
+              for (i = start; i < end; i++) {
+                table_ele = table_ele + body[i]
+              }
+              callback(table_ele)
+            }
+          }
+        })
       }
+      getpage("https://github.com/edugieun/Algorithm-Solving/blob/master/Array/0000_Bomber1(D3%2C%20Matrix)/bomber1.py", this.getdata)
+
+
+      // let table_ele = ''
+      // request({
+      //   url: this.selectedProblem.code_url,
+      // }, function (error, response, body) {
+      //   let start = 0
+      //   let end = 0
+      //   // console.log(body)
+      //   for (let i = 0; i < body.length; i++) {
+      //     if (body[i] === '<' && body[i + 1] === 't' && body[i + 2] === 'a' && body[i + 3] === 'b' && body[i +4] === 'l') {
+      //       start = i
+      //     }
+      //     if (body[i] === '/' && body[i + 1] === 't' && body[i + 2] === 'a' && body[i + 3] === 'b' && body[i +4] === 'l') {
+      //       end = i + 8
+      //       // let table_ele = ''
+      //       for (i = start; i < end; i++) {
+      //         table_ele = table_ele + body[i]
+      //       }
+      //       // console.log(table_ele)
+      //       // console.log(this.codeHtml)
+      //       // this.codeHtml = table_ele
+      //       this.codeSave(3)
+
+      //       break
+      //     }
+      //   }
+      // })
+    }
   }
 </script>
 
